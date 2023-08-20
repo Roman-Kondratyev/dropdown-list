7d418b6684330280f4afee82d78d86f4565f8c41
Roman-Kondratyev <romankondratyev10@gmail.com>
2023-08-20 20:07:20 +0300

    Add JS

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-// const active = document.querySelectorAll(`active`);
-
-// function show(){
-//      const wrapper = document.querySelector(`wrapper`)
-//     if(wrapper.hidden=== true){
-//         wrapper.hidden= false ;
-//         img.stily ='background-image: url(./assets/Chevron2.svg)';
-//     }else{
-//         wrapper.hidden=== true;
-//         img.stily ='background-image: url(./assets/Chevron.svg)';
-//     }
-// }
-
-
-
-// const par = document.querySelectorAll('.wrapper');
-
-// par.addEventListener(`click`, function(event){
-// let paragraff = document.querySelector
-
-
-// })
 
 const active = document.querySelector(".active");
 
@@ -46,29 +24,3 @@ par.addEventListener("click", function (event) {
   paragraff.textContent = event.target.textContent;
   show();
 });
-
-// const active = document.querySelector('.active');
-
-// function show() {
-//     const wrapper = document.querySelector('.wrapper')
-//     let img = document.querySelector('.img')
-//     if (wrapper.hidden === true) {
-//         wrapper.hidden = false;
-//         img.style = 'background-image: url(./assets/Chevron2.svg)'
-//     } else {
-//         wrapper.hidden = true;
-//         img.style = 'background-image: url(./assets/Chevron.svg)'
-//     }
-// }
-
-// active.addEventListener('click', function () {
-//     show()
-// });
-
-// const par = document.querySelector('.wrapper');
-
-// par.addEventListener('click', function (event) {
-//     let paragraph = document.querySelector('.active p');
-//     paragraph.textContent = event.target.textContent;
-//     show()
-// })
